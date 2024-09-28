53369ed52151e1e28fe0eb9830d4fd267272bb15
Fiza <fizajjoiya@gmail.com>
2024-09-28 23:08:11 +0100

    menuitem table

--- src/controllers/menuItem.controller.ts
+++ src/controllers/menuItem.controller.ts
@@ -1,29 +1,69 @@
 import { Request, Response } from "express";
 import MenuItem from "../modals/menuitem.modals";
+import mongoose from "mongoose";
+import cloudinary from "cloudinary";
+
+interface MulterFiles {
+  [fieldname: string]: Express.Multer.File[];
+}
+
+export const getMenuItemData = async (req: Request, res: Response) => {
+  try {
+    const { menuId } = req.params;
+    console.log(menuId);
+    if (!mongoose.Types.ObjectId.isValid(menuId)) {
+      console.log("here");
+      return res.status(404).json({ message: "No Such Menu Found!" });
+    }
+    console.log("not in if statement");
+    const data = await MenuItem.find({ menuId: menuId });
+    console.log("data", data);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching menu item", error });
+  }
+};
 
 export const createMenuItem = async (req: Request, res: Response) => {
   try {
     const { menuId } = req.params;
 
-    if (!menuId) {
+    if (!mongoose.Types.ObjectId.isValid(menuId)) {
       return res.status(404).json({ message: "No Such Menu Found!" });
     }
+    console.log("here");
 
     const existingMenuItem = await MenuItem.findOne({
       menuId: menuId,
       itemName: req.body.itemName,
     });
 
+    console.log("existingMenuItem", existingMenuItem);
+
     if (existingMenuItem) {
       return res.status(409).json({ message: "This Menu Item already Exists" });
     }
 
+    // handling image upload
+    const files = req.files as MulterFiles | undefined;
+    const menuItemImage = files?.menuItemImageFile?.[0];
+
+    if (!menuItemImage) {
+      return res.status(400).json({ message: "Image Field is required" });
+    }
+
+    // Upload main restaurant image
+    const base64Image = Buffer.from(menuItemImage.buffer).toString("base64");
+    const dataURI = `data:${menuItemImage.mimetype};base64,${base64Image}`;
+    const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+
     const newItem = new MenuItem({
       ...req.body,
-      itemPrice: parseFloat(req.body.itemPrice),
+      menuItemImageFile: uploadResponse.url,
       extras: JSON.parse(req.body.extras || "[]"),
-      menuItemImageFile: req.file ? req.file.path : null,
     });
+
+    console.log("newItem", newItem);
 
     await newItem.save();
     res.status(201).json(newItem);
--- src/routes/menuItem.routes.ts
+++ src/routes/menuItem.routes.ts
@@ -1,14 +1,26 @@
 import express from "express";
-import { createMenuItem } from "../controllers/menuItem.controller";
+import {
+  createMenuItem,
+  getMenuItemData,
+} from "../controllers/menuItem.controller";
+import multer from "multer";
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
 // check auth, extract auth token, get current user's data
 // router.get("/all", getAllMenuItemData);
-// router.get("/:id", getMenuItemData);
+router.get("/:menuId", getMenuItemData);
 
 // check auth, create new user
-router.post("/add/:menuId", createMenuItem);
+router.post("/add/:menuId", upload.single("menuItemImageFile"), createMenuItem);
 
 // check auth, extract auth token, validate access token, update user
 // router.put("/", updateMenuItem);
